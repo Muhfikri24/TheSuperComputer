7443b48ac2f28ab0a872e8012b05845918b97a32
Muhfikri24 <Muhammad.3062@students.amikom.ac.id>
2020-03-16 14:45:09 +0700

    Add project files.

---
+++ TheSuperComputer/Vga.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSuperComputer
+{
+    class Vga
+    {
+        private String brand;
+        private int graphicsClock;
+        private int memoryClock;
+
+        public Vga(String brand,int grapicsClock,int memoryClock)
+        {
+            this.brand = brand;
+            this.graphicsClock = grapicsClock;
+            this.memoryClock = memoryClock;
+        }
+        public String getBrand()
+        {
+            return this.brand;
+        }
+        public int getGrapicsClock()
+        {
+            return this.graphicsClock;
+        }
+        public int getMemoryClock()
+        {
+            return this.memoryClock;
+        }
+        public override string ToString()
+        {
+            return $"Vga brand : {this.brand} ; Vga clock grapics : {this.graphicsClock} ; Vga clock memory : {this.memoryClock}";
+        }
+    }
+}
